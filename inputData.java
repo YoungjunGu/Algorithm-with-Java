1a3e803b37e7d81d362fedce226bfe4c05f70700
Young-Jun Gu <33486820+gaki2745@users.noreply.github.com>
2019-03-09 01:56:55 +0900

    Create inputData.java

---
+++ inputData.java
@@ -0,0 +1,100 @@
+static class sc {
+
+		private static BufferedReader br;
+
+		private static StringTokenizer st;
+
+
+
+
+		static void init() {
+
+			br = new BufferedReader(new InputStreamReader(System.in));
+
+			st = new StringTokenizer("");
+
+		}
+
+
+
+
+		static String readLine() {
+
+			try {
+
+				return br.readLine();
+
+			} catch (IOException e) {
+
+			}
+
+			return null;
+
+		}
+
+
+
+
+		static String readLineReplace() {
+
+			try {
+
+				return br.readLine().replaceAll("\\s+", "");
+
+			} catch (IOException e) {
+
+			}
+
+			return null;
+
+		}
+
+
+
+
+		static String next() {
+
+			while (!st.hasMoreTokens()) {
+
+				try {
+
+					st = new StringTokenizer(br.readLine());
+
+				} catch (IOException e) {
+
+				}
+
+			}
+
+			return st.nextToken();
+
+		}
+
+
+
+
+		static long nextLong() {
+
+			return Long.parseLong(next());
+
+		}
+
+
+
+
+		static int nextInt() {
+
+			return Integer.parseInt(next());
+
+		}
+
+
+
+
+		static double nextDouble() {
+
+			return Double.parseDouble(next());
+
+		}
+
+	}
