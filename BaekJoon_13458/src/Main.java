e163b73bc2f6872e4995c385d7026ffa27f6d56e
gaki2745 <guyeongjun@naver.com>
2019-03-09 01:36:58 +0900

    백준 시험감독

---
+++ BaekJoon_13458/src/Main.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int n;
+	static int[] array;
+	static int b,c;
+	static int ans;
+	
+	
+	public static void solving() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		array = new int[n];
+		
+		for (int i = 0 ; i < n ; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		for(int i = 0 ; i < n; i ++) {
+			ans++;
+			
+			array[i] -= b;
+			
+			if (array[i] > 0 ) {
+				ans += (int) Math.ceil((double) array[i] / c);
+			}
+			
+		}
+		
+		System.out.print(ans);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new Main().solving();
+	}
+
+}
