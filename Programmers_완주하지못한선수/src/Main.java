43a7694b07519f1f7ee98a5e68e65a449521dfbf
gaki2745 <guyeongjun@naver.com>
2019-03-26 13:33:40 +0900

    완주하지못한선수 구현

--- Programmers_완주하지못한선수/src/Main.java
+++ Programmers_완주하지못한선수/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	
+	public static String solutions(String[] participants, String[] completion) {
+		int i = 0;
+		Arrays.sort(participants);
+		Arrays.sort(completion);
+		
+		for(i = 0 ; i < completion.length; i++) {
+			if(!participants[i].equals(completion[i])) {
+				return participants[i];
+			}
+		}
+		return participants[i];
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
