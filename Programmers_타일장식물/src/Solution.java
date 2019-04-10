cf20630559e8910fef7a89f08f1185991f2db351
gaki2745 <guyeongjun@naver.com>
2019-04-11 01:25:52 +0900

    타일장식물 (DP)

---
+++ Programmers_타일장식물/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+	
+    public long solution(int N) {
+        long answer = 0;
+        int[] dp = new int[81];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2 ; i <= N ; i++) {
+        	dp[i] = dp[i-1] + dp[i -2];
+        }
+        
+        answer = dp[N]*2 + dp[N-1]*2;
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new Solution().solution(6));
+	}
+
+}
